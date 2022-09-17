6bcaa015a89351e1e44a9c57a986d4bf213b90bc
blandco <jbland@gmail.com>
2022-09-17 15:17:43 -0500

    adds TextView object

--- app/src/main/java/com/joelbland/shades/MainActivity.java
+++ app/src/main/java/com/joelbland/shades/MainActivity.java
@@ -1,19 +1,29 @@
 package com.joelbland.shades;
 
-import android.graphics.Point;
+import android.view.View;
 import android.graphics.Color;
 import android.os.Bundle;
+
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+
+import android.widget.TextView;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.Space;
+
+import com.google.android.material.button.MaterialButtonToggleGroup;
 
 public class MainActivity extends AppCompatActivity {
-    private Button [][] buttons;
+    private TextView status;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
-        // setContentView( R.layout.activity_main );
+        // Call the buildGuiByCode method to create the GUI.
         buildGuiByCode( );
     }
 
@@ -24,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
         // Get screen dimensions
         int screenHeight = getWindowManager().getCurrentWindowMetrics().getBounds().height();
         int screenWidth = getWindowManager().getCurrentWindowMetrics().getBounds().width();
+
+        // Set the height for each button
         int buttonHeight = screenHeight / 6;
 
         // Create the layout manager as a GridLayout
@@ -32,16 +44,46 @@ public class MainActivity extends AppCompatActivity {
         gridLayout.setRowCount( ROWS );
         gridLayout.setBackgroundColor(Color.parseColor("#c89b6d"));
 
+        // Create buttons and add them to gridLayout
+        Button [] button = new Button[3];
+        for( int i=0; i < 3; i++) {
+            button[i] = new Button(this);
+            button[i].setText("1");
+            button[i].setTextSize(28);
+            //button[i].setBackgroundColor(Color.parseColor("#AC7D50"));
+            gridLayout.addView(button[i], screenWidth, buttonHeight);
+        }
+        status = new TextView( this );
+
+
+        // set up status' characteristics
+        status.setWidth(screenWidth);
+        status.setHeight(350);
+        status.setGravity(View.TEXT_ALIGNMENT_CENTER);
+        status.setBackgroundColor( Color.GREEN );
+        status.setTextSize(28);
+        status.setText("HELLO, Jerry!");
+        gridLayout.addView(status);
+
+
         // Create the buttons and add them to gridLayout
-        buttons = new Button[ROWS][COLUMNS];
+/*        buttons = new Button[ROWS][COLUMNS];
+
         for( int row = 0; row < ROWS; row++ ) {
             for( int col = 0; col < COLUMNS; col++ ) {
                 buttons[row][col] = new Button( this );
+                buttons[row][col].setBackgroundColor(Color.parseColor("#ac7d50"));
                 gridLayout.addView( buttons[row][col], screenWidth, buttonHeight );
+
             }
-        }
+        }*/
 
         // Set gridLayout as the View of this Activity
         setContentView( gridLayout );
+    }
+    private class ButtonHandler implements View.OnClickListener {
+        public void onClick( View v ) {
+
+        }
     }
 }
