bc15dc7532e8c8597dd007e2b6bf68e55e3fb061
blandco <jbland@gmail.com>
2022-09-17 21:45:36 -0500

    1. create strings programmatically 2. fixes button margins 3. adds comments

--- app/src/main/java/com/joelbland/shades/MainActivity.java
+++ app/src/main/java/com/joelbland/shades/MainActivity.java
@@ -29,59 +29,66 @@ public class MainActivity extends AppCompatActivity {
         ConstraintLayout layout = findViewById(R.id.mainActivity);
         int ROWS = 3;
         int COLUMNS = 1;
+        int TV_HEIGHT = 350;
+        int BUTTON_TEXTSIZE = 28;
+        int TV_TEXTSIZE = 18;
 
-        String goldIs = getResources().getString(R.string.gold_is);
-        String plumIs = getResources().getString(R.string.plum_is);
-        String blueIs = getResources().getString(R.string.blue_is);
-        String GOLD = getResources().getString(R.string.GOLD);
-        String PLUM = getResources().getString(R.string.PLUM);
-        String BLUE = getResources().getString(R.string.BLUE);
-        String buttonBackground = getResources().getString(R.string.button_background);
-        String viewBackground = getResources().getString(R.string.view_background);
+        // Change the App Label (top left) to “HW3 Ex 2” using a strings.xml resource
+        this.setTitle(getResources().getString(R.string.appLabel));
 
         // Get screen dimensions
         int screenHeight = getWindowManager().getCurrentWindowMetrics().getBounds().height();
         int screenWidth = getWindowManager().getCurrentWindowMetrics().getBounds().width();
 
-        // Set the height for each button
-        int buttonHeight = screenHeight / 6;
+        // Set the height for each button based on the screen height
+        int buttonHeight = screenHeight / (ROWS * 2);
 
         // Create the layout manager as a GridLayout
         GridLayout gridLayout = new GridLayout( this );
         gridLayout.setColumnCount( COLUMNS );
         gridLayout.setRowCount( ROWS );
-        gridLayout.setBackgroundColor(Color.parseColor(viewBackground));
+        gridLayout.setUseDefaultMargins(true); // use this method so the buttons don't look weird
+        gridLayout.setPadding(10,10,10,10);
+        // Change the background color to “#C89B6D” using a strings.xml resource
+        gridLayout.setBackgroundColor(Color.parseColor(getResources().getString(R.string.view_background)));
 
-        // Create buttons and add them to gridLayout
+        // Create an array of buttons and add them to gridLayout
         Button [] button = new Button[3];
 
+        // Loop through the array and create buttons
         for( int i=0; i < 3; i++) {
             button[i] = new Button(this);
+            // Set attributes for each button
             button[i].setId(View.generateViewId());
+            // Set a Tag to identify which button is clicked in the OnClickListener
             button[i].setTag("btn" + i);
-            button[i].setTextSize(28);
-            //button[i].setBackgroundColor(Color.parseColor(buttonBackground));
+            button[i].setTextSize(BUTTON_TEXTSIZE);
+            // Connect the OnClickListener method to each button
             button[i].setOnClickListener(btnClick);
-            //button[i].setBackgroundColor(Color.parseColor(buttonBackground));
+            // Change the background color to “#AC7D50” using a strings.xml resource
+            button[i].setBackgroundColor(Color.parseColor(getResources().getString(R.string.button_background)));
+            // Add each button to the GridLayout
             gridLayout.addView(button[i], screenWidth, buttonHeight);
         }
 
-        // Set each button text
-        button[0].setText(PLUM);
-        button[1].setText(BLUE);
-        button[2].setText(GOLD);
+        // Set each button's text
+        button[0].setText(getResources().getString(R.string.PLUM));
+        button[1].setText(getResources().getString(R.string.BLUE));
+        button[2].setText(getResources().getString(R.string.GOLD));
 
         // Create a TextView to show the output
         status = new TextView( this );
-        // set up status' characteristics
+        // Set the TextView attributes
         status.setWidth(screenWidth);
-        status.setHeight(350);
+        status.setHeight(TV_HEIGHT);
         status.setGravity(Gravity.CENTER);
-        status.setBackgroundColor(Color.parseColor(buttonBackground));
-        status.setTextSize(14);
+        status.setBackgroundColor(Color.parseColor(getResources().getString(R.string.button_background)));
+        status.setTextSize(TV_TEXTSIZE);
 
         // Set the status text to null to begin
         status.setText("");
+        // Set the TextView to invisible to begin
+        status.setVisibility(View.INVISIBLE);
 
         // Add the TextView to the GridLayout
         gridLayout.addView(status);
@@ -91,9 +98,13 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    // OnClickListener for the buttons
     private View.OnClickListener btnClick = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
+            // Set the TextView to visible
+            status.setVisibility(View.VISIBLE);
+            // Get the button's Tag from view and set the output string accordingly
             switch((String) view.getTag()){
                 case "btn0":
                     status.setText(getResources().getString(R.string.plum_is));
